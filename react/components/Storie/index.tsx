385b7c3e3443ac47e47546ed26a91fad2c12591b
Ygor Neves <contato@ygorneves.com>
2020-09-25 00:17:55 -0300

    Add images to stories

--- react/components/Storie/index.tsx
+++ react/components/Storie/index.tsx
@@ -12,10 +12,7 @@ const StoriesComponent = () => {
 
     const storieAction = (action: Function): any =>
         setTimeout(() => {
-            action('pause')
-            setTimeout(() => {
-                action('play')
-            }, time)
+            action('play')
         }, time)
 
     const stories = [
@@ -28,6 +25,36 @@ const StoriesComponent = () => {
                 return (
                     <img
                         src="https://i.picsum.photos/id/133/1080/1920.jpg?hmac=mw1-3qObjR9g0YZA8jbk2BvlgH6t4o1xWCCT44KC9PA"
+                        width="100%"
+                        alt="Imagem"
+                    />
+                )
+            },
+        },
+        {
+            content: ({ action }: Content) => {
+                useEffect(() => {
+                    storieAction(action)
+                }, [])
+
+                return (
+                    <img
+                        src="https://i.picsum.photos/id/15/1080/1920.jpg?hmac=fVtH2bkFLY2ifCJ7-1DNqSXoH2nmDeOEYsdgXsLVSjk"
+                        width="100%"
+                        alt="Imagem"
+                    />
+                )
+            },
+        },
+        {
+            content: ({ action }: Content) => {
+                useEffect(() => {
+                    storieAction(action)
+                }, [])
+
+                return (
+                    <img
+                        src="https://i.picsum.photos/id/485/1080/1920.jpg?hmac=bylnefLFXgDQo5wKMp5p5jYCzJJaJ_a3lGpat-NW7y0"
                         width="100%"
                         alt="Imagem"
                     />
