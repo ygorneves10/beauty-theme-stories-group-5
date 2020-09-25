a36377454ab552f0b588a179eb5d3598446ebede
Matthaus <matthauszigillig@hotmail.com>
2020-09-25 11:43:33 -0300

    feat: query products

--- react/components/Storie/index.tsx
+++ react/components/Storie/index.tsx
@@ -1,66 +1,85 @@
 import React, { useEffect } from 'react'
 import { NoSSR } from 'vtex.render-runtime'
+import { useQuery } from 'react-apollo'
+import { product as queryProduct } from 'vtex.store-resources/Queries'
 
 interface Content {
-    action: Function
-    isPaused?: boolean
+  action: Function
+  isPaused?: boolean
 }
 
 interface StorieProps {
-    img: string
-    behavior: string
+  img: string
+  behavior: string
 }
 
-interface StorieComponentProps extends Content, StorieProps { }
+interface StorieComponentProps extends Content, StorieProps {}
 
 const StorieComponent = ({ action, img, behavior }: StorieComponentProps) => {
-    const time = 7000
+  const time = 7000
 
-    const storieAction = (action: Function): any => setTimeout(() => { action('play') }, time)
+  const storieAction = (action: Function): any =>
+    setTimeout(() => {
+      action('play')
+    }, time)
 
-    useEffect(() => {
-        console.log("BEHAVIOR => ", behavior)
-        storieAction(action)
-    }, [])
+  useEffect(() => {
+    console.log('BEHAVIOR => ', behavior)
+    storieAction(action)
+  }, [])
 
-    return (
-        <img
-            src={img}
-            width="100%"
-            alt="Imagem"
-        />
-    )
+  const { loading, data, error } = useQuery(queryProduct, {
+    variables: {
+      identifier: {
+        field: 'id',
+        value: sku,
+      },
+    },
+  })
+
+  if (loading || error || !data) {
+    return null
+  }
+
+  console.log(data.product)
+
+  return <img src={img} width="100%" alt="Imagem" />
 }
 
 const StoriesComponent = () => {
-    const Stories = React.lazy(() => import('react-insta-stories'))
+  const Stories = React.lazy(() => import('react-insta-stories'))
 
-    const stories = [
-        {
-            img: "https://i.picsum.photos/id/133/1080/1920.jpg?hmac=mw1-3qObjR9g0YZA8jbk2BvlgH6t4o1xWCCT44KC9PA",
-            behavior: "add-to-cart"
-        },
-        {
-            img: "https://i.picsum.photos/id/15/1080/1920.jpg?hmac=fVtH2bkFLY2ifCJ7-1DNqSXoH2nmDeOEYsdgXsLVSjk",
-            behavior: "swipe-up"
-        },
-        {
-            img: "https://i.picsum.photos/id/485/1080/1920.jpg?hmac=bylnefLFXgDQo5wKMp5p5jYCzJJaJ_a3lGpat-NW7y0",
-            behavior: "add-to-cart"
-        }
-    ].map(({ img, behavior }: StorieProps) => (
-        {
-            content: ({ action }: Content) => <StorieComponent action={action} img={img} behavior={behavior} />
-        }
-    ))
+  const stories = [
+    {
+      img:
+        'https://i.picsum.photos/id/133/1080/1920.jpg?hmac=mw1-3qObjR9g0YZA8jbk2BvlgH6t4o1xWCCT44KC9PA',
+      behavior: 'add-to-cart',
+    },
+    {
+      img:
+        'https://i.picsum.photos/id/15/1080/1920.jpg?hmac=fVtH2bkFLY2ifCJ7-1DNqSXoH2nmDeOEYsdgXsLVSjk',
+      behavior: 'swipe-up',
+    },
+    {
+      img:
+        'https://i.picsum.photos/id/485/1080/1920.jpg?hmac=bylnefLFXgDQo5wKMp5p5jYCzJJaJ_a3lGpat-NW7y0',
+      behavior: 'add-to-cart',
+    },
+  ].map(({ img, behavior }: StorieProps) => ({
+    content: ({ action }: Content) => (
+      <StorieComponent action={action} img={img} behavior={behavior} />
+    ),
+  }))
 
-    return (
-        <NoSSR>
-            <React.Suspense fallback={<div>Carregando...</div>}>
-                {stories?.length && <Stories loop stories={stories} width={432} height={768} />}
-            </React.Suspense>
-        </NoSSR>
-    )
+  return (
+    <NoSSR>
+      <React.Suspense fallback={<div>Carregando...</div>}>
+        {stories?.length && (
+          <Stories loop stories={stories} width={432} height={768} />
+        )}
+      </React.Suspense>
+    </NoSSR>
+  )
 }
 
 export default StoriesComponent
