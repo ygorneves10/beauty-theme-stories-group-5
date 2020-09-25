1d2c461be670e6f80aa6cd33b874b95cb7261d9c
Ygor Neves <contato@ygorneves.com>
2020-09-25 15:19:43 -0300

    Add swipe up behavior

--- react/components/Storie/index.tsx
+++ react/components/Storie/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NoSSR } from 'vtex.render-runtime'
 import { useCssHandles } from 'vtex.css-handles'
 // import { useQuery } from 'react-apollo'
 // import { product as queryProduct } from 'vtex.store-resources/Queries'
+import { Swipe } from "react-swipe-component"
 
 interface Content {
     action: Function
@@ -14,6 +15,7 @@ interface StorieComponentProps extends Content, StorieItem { }
 interface StorieItem {
     sku: string
     image: string
+    setCurrentStorie: Function
     behavior: "swipe-up" | "add-to-cart"
 }
 
@@ -27,51 +29,82 @@ interface StoriesComponentProps {
     stories: Array<Storie>
 }
 
-const StorieComponent = ({ action, image, behavior, sku }: StorieComponentProps) => {
+const StorieComponent = ({ action, image, behavior, sku, setCurrentStorie }: StorieComponentProps) => {
     const time = 7000
 
-    const storieAction = (action: Function): any =>
+    const storieAction = (action: Function): any => {
         setTimeout(() => {
+            setCurrentStorie({ sku, behavior })
             action('play')
         }, time)
+    }
 
     useEffect(() => {
-        console.log('BEHAVIOR => ', behavior)
-        console.log('SKU => ', sku)
         storieAction(action)
     }, [])
 
-    return <img src={image} width="100%" alt="Imagem" />
+    return (
+        <img src={image} width="100%" alt="Imagem" />
+    )
 }
 
 const StoriesComponent = (props: StoriesComponentProps) => {
     const Stories = React.lazy(() => import('react-insta-stories'))
+
+    const [storieOpen, setStorieOpen] = useState(0)
+    const [currentStorie, setCurrentStorie] = useState({
+        sku: null,
+        behavior: null
+    })
+
     const { stories } = props
 
     const CSS_HANDLES = [
-        'stories'
+        'stories',
+        'swipe'
     ] as const
 
     const handles = useCssHandles(CSS_HANDLES)
 
-    return stories?.length ? stories.map(({ image, title, storiesItems }: Storie) => {
+    const onSwipeUpListener = () => {
+        console.log("Swiped Up", currentStorie)
+        if (currentStorie.behavior === "swipe-up") {
+            console.log("Swiped Up", true)
+        }
+    }
+
+    return stories?.length ? stories.map((storie, index) => {
+        const { image, title, storiesItems }: Storie = storie
         const storiesArr = storiesItems.map(({ image, behavior, sku }: StorieItem) => (
             {
-                content: ({ action }: Content) => <StorieComponent action={action} image={image} behavior={behavior} sku={sku} />
+                content: ({ action }: Content) => <StorieComponent action={action} image={image} behavior={behavior} sku={sku} setCurrentStorie={setCurrentStorie} />
             }
         ))
 
         return (
             <>
-                <img src={image} />
-                <p>{title}</p>
-                <div className={handles.stories}>
-                    <NoSSR onSSR={() => null}>
-                        <React.Suspense fallback={<div>Carregando...</div>}>
-                            {storiesArr?.length && <Stories loop stories={storiesArr} width={432} height={768} />}
-                        </React.Suspense>
-                    </NoSSR>
+                <div onClick={() => setStorieOpen(index + 1)}>
+                    <img src={image} />
+                    <p>{title}</p>
                 </div>
+                {
+                    storieOpen && storieOpen === index + 1 ? (
+                        <div className={handles.stories}>
+                            <Swipe
+                                nodeName="div"
+                                className={handles.swipe}
+                                onSwipedDown={() => setStorieOpen(0)}
+                                onSwipedUp={onSwipeUpListener}
+                            >
+                                <NoSSR onSSR={() => null}>
+                                    <React.Suspense fallback={<div>Carregando...</div>}>
+                                        {storiesArr?.length && <Stories loop stories={storiesArr} width={432} height={768} />}
+                                    </React.Suspense>
+                                </NoSSR>
+                            </Swipe>
+                        </div>
+                    ) : null
+                }
             </>
         )
     }) : null
